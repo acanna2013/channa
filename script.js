0f816b08ad44c17521e65b7b9e5b1be39b8bfbe9
Anna Chen <51142303+acanna2013@users.noreply.github.com>
2023-09-03 01:46:53 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function cubeRotation() {
+  const cube = document.querySelector(".cube");
+  let mouseX = 0;
+  let mouseY = 0;
+  const rotationValue = 270;
+
+  const handleMouseMove = (event) => {
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+    rotateX = -(mouseY / window.innerHeight - 0.5) * rotationValue;
+    rotateY = (mouseX / window.innerWidth - 0.5) * rotationValue;
+    cube.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+  };
+  window.addEventListener("mousemove", handleMouseMove);
+};
+
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+};
